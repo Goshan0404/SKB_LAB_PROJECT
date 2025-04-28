3f6427ea2b169a79a242619277498638908b7fe2
Georgy <chernihov.g@gmail.com>
2025-04-28 13:46:08 +0500

    OrderRepository added

--- src/main/java/sinara_project/repositories/OrderRepository.java
+++ src/main/java/sinara_project/repositories/OrderRepository.java
@@ -2,8 +2,8 @@ package sinara_project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import sinara_project.models.Order;
+import sinara_project.models.UserOrder;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<UserOrder, Long> {
 }
