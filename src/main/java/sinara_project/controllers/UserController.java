78ad73b64620181ad537a0ac124aa0f36e8685b3
Georgy <chernihov.g@gmail.com>
2025-04-28 13:44:55 +0500

    user repository, service, added

--- src/main/java/sinara_project/controllers/UserController.java
+++ src/main/java/sinara_project/controllers/UserController.java
@@ -1,26 +1,27 @@
 package sinara_project.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import sinara_project.models.UserApp;
+import sinara_project.models.user.UserApp;
+import sinara_project.models.user.UserRegisterDto;
 import sinara_project.service.UserService;
 
 @RestController
 public class UserController {
 
     @Autowired
-    UserService userService;
+    private UserService userService;
 
     @PostMapping("/register")
-    public UserApp register(@RequestBody UserApp user) {
+    public UserApp register(@Valid @RequestBody UserRegisterDto user) {
         return userService.register(user);
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody UserApp user) {
+    public String login(@RequestBody UserRegisterDto user) {
         return userService.verify(user);
     }
 }
---
+++ src/main/java/sinara_project/service/UserService.java
@@ -0,0 +1,47 @@
+package sinara_project.service;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import sinara_project.models.user.UserApp;
+import sinara_project.models.user.UserRegisterDto;
+import sinara_project.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JWTService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    // убарть 12 в констатнту как и в конфиге
+    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+
+    public UserApp register(UserRegisterDto user) {
+        UserApp userApp = modelMapper.map(user, UserApp.class);
+        userApp.setPassword(bCryptPasswordEncoder.encode(userApp.getPassword()));
+        return userRepository.save(userApp);
+    }
+
+    public String verify(UserRegisterDto user) {
+        Authentication authentication = authenticationManager
+                .authenticate(
+                        new UsernamePasswordAuthenticationToken(user.getName(), user.getPassword()));
+
+        if (!authentication.isAuthenticated())
+            return "fail";
+        return jwtService.generate(user.getName());
+    }
+}
