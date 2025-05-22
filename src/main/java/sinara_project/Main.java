cd4fb79fa934e90b6f7ef8e5e60a3532b54e892c
Georgy <chernihov.g@gmail.com>
2025-05-22 20:39:59 +0500

    EnableMethodSecurity

--- src/main/java/sinara_project/Main.java
+++ src/main/java/sinara_project/Main.java
@@ -3,8 +3,10 @@ package sinara_project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(jsr250Enabled = true)
 @EnableScheduling
 public class Main {
     public static void main(String[] args) {
