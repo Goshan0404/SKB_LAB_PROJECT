2d0f3a78706ff395d76eb5e732a2e8a773773734
Georgy <chernihov.g@gmail.com>
2025-05-22 23:27:24 +0500

    api call limits bags changes

--- src/main/java/sinara_project/aspect/ApiCallLimitAspect.java
+++ src/main/java/sinara_project/aspect/ApiCallLimitAspect.java
@@ -1,38 +1,40 @@
-//package sinara_project.aspect;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.aspectj.lang.JoinPoint;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Before;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.concurrent.atomic.AtomicInteger;
-//
-//@Slf4j
-//@Aspect
-//@Component
-//public class ApiCallLimitAspect {
-//
-//    @Value("${api.max.calls}")
-//    private int maxApiCalls;
-//
-//    private final AtomicInteger apiCallCount = new AtomicInteger(0);
-//
-//    @Before("execution(* com.sinara_project.controller.OrderController.*(..))")
-//    public void checkApiCallLimit(JoinPoint joinPoint) throws Exception {
-//        int currentCount = apiCallCount.incrementAndGet();
-//
-//        if (currentCount > maxApiCalls) {
-//            throw new Exception("Api call limit exceed");
-//        }
-//
-//        log.info("Current Api calls:" + currentCount);
-//    }
-//
-//    @Scheduled(fixedRate = 1800000)
-//    public void resetApiCallCount() {
-//        apiCallCount.set(0);
-//    }
-//}
+package sinara_project.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class ApiCallLimitAspect {
+
+    @Value("${api.max.calls}")
+    private int maxApiCalls;
+
+    @Autowired
+    private RequestCounter requestCounter;
+
+    @Pointcut("execution(* com.example.start.controller.ApiController.*(..)))")
+    public void apiMethods() {
+    }
+
+    @Around("apiMethods()")
+    public Object checkLimit(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        String methodName = joinPoint.getSignature().toShortString();
+        int currentCount = requestCounter.getCount(methodName);
+        requestCounter.increment(methodName);
+
+        if (currentCount >= maxApiCalls) {
+            log.info("Лимит для метода: ", methodName);
+            return null;
+        }
+        return joinPoint.proceed();
+    }
+}
