e693244b54ac48b2454277c4159e528503b77d32
Georgy <chernihov.g@gmail.com>
2025-05-22 21:17:54 +0500

    get orders by id

--- src/main/java/sinara_project/controllers/OrderController.java
+++ src/main/java/sinara_project/controllers/OrderController.java
@@ -1,11 +1,13 @@
 package sinara_project.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import sinara_project.metrics.UserOrderMetrics;
 import sinara_project.models.order.UserOrderDto;
+import sinara_project.repositories.OrderRepository;
 import sinara_project.service.OrderServiceProducer;
 
 
@@ -16,11 +18,19 @@ public class OrderController {
     private OrderServiceProducer orderServiceProducer;
 
     @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
     private UserOrderMetrics metrics;
 
     @PostMapping("/order/create")
     public void createOrder(@RequestBody UserOrderDto order) {
         orderServiceProducer.createOrder(order);
         metrics.increment(order);
+    }
+
+    @GetMapping("order")
+    public void getUserOrders(@RequestBody Long userId) {
+        orderRepository.findAllByUserId(userId);
     }
 }
--- src/main/java/sinara_project/repositories/OrderRepository.java
+++ src/main/java/sinara_project/repositories/OrderRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sinara_project.models.order.UserOrder;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<UserOrder, Long> {
+    List<UserOrder> findAllByUserId(Long userId);
 }
