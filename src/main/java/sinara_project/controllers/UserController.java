7c166f391c1b91ac6291bedee017c93b0edd0fa4
chern <chernihov.g@gmail.com>
2025-05-28 15:35:52 +0500

    get and delete profile added

--- src/main/java/sinara_project/controllers/UserController.java
+++ src/main/java/sinara_project/controllers/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import sinara_project.models.user.UserApp;
+import sinara_project.models.user.UserDto;
 import sinara_project.models.user.UserRegisterDto;
 import sinara_project.service.UserService;
 
@@ -16,12 +16,22 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public UserApp register(@Valid @RequestBody UserRegisterDto user) {
+    public UserDto register(@Valid @RequestBody UserRegisterDto user) {
         return userService.register(user);
     }
 
     @PostMapping("/login")
     public String login(@Valid @RequestBody UserRegisterDto user) {
         return userService.verify(user);
+    }
+
+    @PostMapping("/profile")
+    public UserDto getProfile(@RequestBody long id) {
+        return userService.getProfile(id);
+    }
+
+
+    public void deleteProfile(@RequestBody long id) {
+        userService.deleteProflie(id);
     }
 }
